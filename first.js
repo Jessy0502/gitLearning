f57fae151ae84327a26e9567da23e7aeb091fb6b
Jessy0502 <871089960@qq.com>
2021-10-24 23:27:37 +0800

    git push

--- first.js
+++ first.js
@@ -1,3 +1,19 @@
 alert('本康熙来了')
 console.log('这是用控制台发出的信息')
 prompt(info)
+//创建全局变量name
+var name = "全局变量";
+
+//创建一个函数
+function fun(){
+    console.log(this.name);
+}
+
+//创建一个对象
+var obj = {
+    name:"孙悟空",
+    sayName:fun
+};
+
+//调用obj.sayName()时可以输出obj的名字
+obj.sayName();
